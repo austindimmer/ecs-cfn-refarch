f1897dcfe2988bc2e9c8fd108f354b808f89f63f
Unknown <adi@emakina.com>
2018-05-12 15:37:23 +0200

    CF is Failing Since Instances Do not Signal Trying ASP Ref Architecture
    https://github.com/aws-samples/aws-ecs-windows-aspnet

---
+++ DockerSitefinity.ps1
@@ -0,0 +1,4 @@
+﻿docker pull microsoft/aspnet:4.7.1-windowsservercore-ltsc2016
+docker build -t sfImage -m 3GB .
+docker run -d -p 8000:80 -m 3GB --name sfImage sfContainer1
+docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" sfContainer1
